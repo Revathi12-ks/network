3dfb68d1b494f5f992336f9b898582e8f010a144
Revathi12-ks <150452970+Revathi12-ks@users.noreply.github.com>
2024-03-14 15:43:54 +0530

    area,reverse,palindrome,set of numbers,quadratic,armstrong,prime

--- odd.sh
+++ odd.sh
@@ -1,11 +1,15 @@
-check_odd_even() {
-if [  $((number%2)) -eq 0 ];
+echo "Enter a set of numbers separated by spaces: "
+read -a numbers
+sum_even=0
+sum_odd=0
+for num in "${numbers[@]}"
+do
+if [ $(($num % 2)) -eq 0 ]
 then
-echo "$number is even"
+sum_even=$(($sum_even + $num))
 else
-echo "$number is odd"
+sum_odd=$(($sum_odd + $num))
 fi
-}
-echo "Please enter a number:"
-read number
-check_odd_even "$number"
+done
+echo "Sum of even numbers is: $sum_even"
+echo "Sum of odd numbers is: $sum_odd"
